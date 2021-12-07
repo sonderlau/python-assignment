eca5d456ce78658e3f161beb727ae7d25b3fd08c
SonderLau <liuless26@gmail.com>
2021-12-07 10:43:08 +0800

    :bug: Fix: 12-03/7-4 test#2
    @2 with no space at the end while others require

--- 12-03/7-4.py
+++ 12-03/7-4.py
@@ -1,27 +1,104 @@
+from typing import Any
+from icecream import ic
+
+class Node:
+    def __init__(self, data: Any) -> None:
+        self.data = data
+        self.next = None
+
+    def __repr__(self) -> str:
+        return f"{self.data}"
+
+
+class LinkedList:
+    
+    def __init__(self) -> None:
+        self.head = None
+        self.length = 0
+
+    def __iter__(self) -> Any:
+        node = self.head
+        while node:
+            yield node.data
+            node = node.next
+
+    def __len__(self) -> int:
+        return self.length
+    
+    def __repr__(self) -> str:
+        return " ".join([str(item) for item in self]) + " "
+
+    def insert_nth(self, index: int, data: Any) -> None:
+        if not 0 <= index <= len(self):
+            return
+
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        elif index == 0:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            temp = self.head
+            for _ in range(index - 1):
+                temp = temp.next
+            new_node.next = temp.next
+            temp.next = new_node
+        self.length += 1
+
+    def insert_tail(self, data: Any) -> None:
+        self.insert_nth(len(self), data)
+        
+    def print_list(self) -> None:
+
+        print(self)
+
+    def delete_nth(self, index: int = 0) -> Any:
+        if not 0 <= index <= len(self) - 1:
+            return
+
+        delete_node = self.head
+
+        if index == 0:
+            self.head = self.head.next
+        else:
+            temp = self.head
+            for _ in range(index - 1):
+                temp = temp.next
+            delete_node = temp.next
+            temp.next = temp.next.next
+        self.length -= 1
+        return delete_node.data
+
+
 tmp = input()
 
-nums = list(map(int, input().split(" ")))
+# init nums
+nums = list(input().split(" "))
 
+# linked list
+link = LinkedList()
+
+for x in nums:
+    link.insert_tail(x)
+
+
+# rounds
 tmp = int(input())
 
 for x in range(0, tmp):
     opt = input()
-    
-    if opt[0:1] == '0':
-        k,d = map(int , opt[2:].split(" "))
-        if k > len(nums):
-            continue
-        nums.insert(k, d)
+
+    # Add
+    if opt[:1] == "0":
+        sp = opt[2:].split(" ")
+        k = int(sp[0])
+        d = sp[1]
+        link.insert_nth(k, d)
+    # Remove
     else:
         k = int(opt[2:])
-        if k <= 0 or k >= len(nums):
-            continue
-        else:
-            nums.remove(nums[k - 1])
-    print(nums)
-            
-for x in nums:
-    if x != nums[0]:
-        print(" ", end="")
+        link.delete_nth(k - 1)
     
-    print(x, end="")
+
+link.print_list()
